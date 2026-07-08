2a54cdc93e22db73a083b0cffaf634208665d7ef
JulitononSocrates <afcthechiz@gmail.com>
2026-07-08 21:10:58 +0200

    My first projects

---
+++ 20260706-hb-jose-backend/server.js
@@ -0,0 +1,108 @@
+//IMPOSTOR HERRAMIENTAS
+const express =require ('express');
+const app =express();
+
+//NUESRTRA BASE DE DATOS FALSA(El Catálogo)
+//Esto es array de objetos
+const inventario = [
+     {id: 1 articulo : "Portátil HP",stock:115};
+     {id:artículo: "Monitor Dell" ,stock:5};
+     {id:articilo: "Teclado mecánico" ,stock:22};
+
+//LA RUTA (Camarero)
+//Cuando alguien pida 'api/productos',le entregamos el inventario.
+
+app.get('api/productos'), (re, res)=> {
+//res.json convierte los datos para que el internet entienda
+res.jon(inventario);
+}]
+
+//ENTENDER EL SERVIDOR
+// Le decimos que escuhe el puerto 3000
+app.listen(3000, () => () 
+ console.log)(servidor encendido y escuchado en puerto 3000); 24 );                                                                              ddddddddddddddddddddddddddddddddddddddddddddddd)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
